dd2586704a199e87a850faeae9000663dcfde79f
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2024-01-26 23:54:03 -0600

    Getting started

---
+++ PetFinder App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PetFinder App
+//
+//  Created by Marco Alonso Rodriguez on 26/01/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PetFinder App/PetFinder_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetFinder_AppApp.swift
+//  PetFinder App
+//
+//  Created by Marco Alonso Rodriguez on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct PetFinder_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
